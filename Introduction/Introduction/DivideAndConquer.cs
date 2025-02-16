a160e25512e7beef0fd9b4d99c917779e18f6457
rameez <rameezkhalil23@gmail.com>
2025-02-16 10:00:17 -0500

    basics of threads and implemented a divde and conquer approach

---
+++ Introduction/Introduction/DivideAndConquer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction; 
+
+public  class DivideAndConquer
+{
+    public static void Main(string[] args) {
+
+
+        int[] arr = { 10, 20, 30, 40 };
+        int sum1 = 0;
+        int sum2 = 0; 
+
+        Thread thread1 = new Thread(() => sum1 = sumArray(arr, 0,arr.Length/2));
+        Thread thread2 = new Thread(() => sum2 = sumArray(arr, arr.Length / 2, arr.Length));
+        
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine($"Total sum result is : {sum1+sum2}");
+    
+    }
+
+
+
+    //DIVDE AND CONQUER APPROACH.
+    public static int sumArray(int[] arr, int start, int end)
+    {
+        int sum = 0;
+        for (int i = start; i < end; i++)
+        {
+            sum+= arr[i];
+        }
+
+        return sum; 
+    }
+
+
+    //LINEAR APPROACH.
+    public static int sum(IEnumerable<int> nums) {
+        var sum = 0;
+
+        foreach (var x in nums)
+        {
+            sum+= x;
+        }
+
+        return sum; 
+
+    } 
+}
+/*
+ * Divide the chunk into multiple portions. 
+ */
---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,76 @@
+﻿class Program
+{
+    //public static void Main(string[] args)
+    //{
+
+    //    //Create a new thread.
+    //    Thread thread1 = new Thread(threadScheduler);
+    //    Thread thread2 = new Thread(threadScheduler);
+
+
+
+    //    thread1.Name = "Theead1";
+    //    thread2.Name = "Thread2"; 
+
+
+    //    thread1.Start();
+    //    thread2.Start();
+    //}
+
+
+    static void WriteThreadId()
+    {
+        Console.WriteLine(Thread.CurrentThread.Name);
+    }
+
+
+
+
+    //THREAD SCHEDULER DEMONSTRATION.
+    public static void threadScheduler()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            WriteThreadId();
+            Thread.Sleep(50);
+        }
+    }
+
+    
+}
+
+
+
+/*
+ * CPU, THREAD, AND THREAD SCHEDULER.
+ * 
+ * CPU - CPU can only run threads, and application always have a thread.
+ *      when the application starts execution, then the thread is assigned to the CPU, via thread scheduler.
+ *      To the CPU nothing exists but a thread.
+ *      Time_slice is the time during which the thread is assigned to the CPU. - It's not under devs control.
+ *      
+ *      
+ *      Thread scheduler is independent of the applications exists, it only cares about the total number of threads.
+ *      
+ *      When there are multiple threads in an application, then that application is termed as 'MultiThreading' application.
+ *      
+ *   
+ * 
+ */
+
+
+
+/*
+ * BASIC SYNTAX.
+ *  Create a thread object.
+ *  Assign a task (pass the function).
+ *  Start the thread.
+ *  
+ */
+
+
+/*
+ * WHY DO WE NEED A THREAD - DIVIDE AND CONQUER.
+ * 
+ */
+
