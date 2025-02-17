84251d175efaed1416ecdb4403b52649bacf8dd7
rameez <rameezkhalil23@gmail.com>
2025-02-17 09:51:57 -0500

    Implemented thread safety, learnt syncrhonization, implemented a ticket booking system, learnt about locks

---
+++ Introduction/ThreadSynchronization/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http.Headers;
+
+class Program
+{
+    private static int number = 0; 
+    private static object lockObject = new object(); //this locks our critical section.
+
+
+    public static void Main(string[] args)
+    {
+        ThreadSynchronizationSafe(); 
+    }
+
+
+
+
+
+    //THREAD SYNCRHONIZATION EXAMPLE 1. (Non atomic operation, and non consistent result due to race condition).
+
+    //increment the counter.
+    public static void counter()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            number++;
+            //non atomic operation (First Read and Second increment) and might not result in accurate result.
+            //each variable instantiate ith variable separately.
+            // number is the shared resource.
+        }
+   
+    }
+    //Increment the counter through threads.
+    public static void ThreadSynchronization()
+    {
+
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
+
+        thread1.Start();
+        thread2.Start();
+        thread1.Join(); 
+        thread2.Join();
+
+        Console.WriteLine($"Final value : {number}" );
+    }
+
+
+    //THREAD SYNCRHONIZATION EXAMPLE 2. (Lock Object, Thread safe).
+
+    public static void CounterThreadSafe()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            lock (lockObject) //try catch finally builtin, the finally releases the lock always.
+            {
+                number++;
+            }
+        }
+    }
+    public static void ThreadSynchronizationSafe()
+    {
+        Thread thread1 = new Thread (CounterThreadSafe);
+        Thread thread2 = new Thread(CounterThreadSafe);
+
+        thread1.Start();
+        thread2.Start();
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine($"The final value: {number}");
+    }
+
+}
+
+
+
+/*
+ * 
+ * TRHREAD SYNCHRONIZATION - 
+ * 
+ * When multiple threads are using a shared resource.
+ * 
+ * 
+ * RACE CONDITION - When multiple threads try to retrieve the shared resouce.
+ */
---
+++ Introduction/ThreadSynchronization/TicketCounter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSynchronization; 
+
+public class TicketCounter
+{
+    private static int ticketCounter = 10; 
+    private static readonly object lockObject = new object();
+
+
+    //decreasing mechanism.
+    public static void TicketBook()
+    {
+        lock (lockObject)
+        {
+            if (ticketCounter > 0)
+            {
+                ticketCounter--; //non atomic operation
+                Console.WriteLine($"Ticket Count {ticketCounter} Booked By {Thread.CurrentThread.Name}");
+            }
+            else
+            {
+                Console.WriteLine("No tickets available");
+            }
+        }
+        
+        
+    }
+
+
+    //logic for threads.
+    public static void BookTicket(int users)
+    {
+
+        Thread[] threads = new Thread[users];
+        for(int i=0; i<users; i++)
+        {
+            threads[i] = new Thread(TicketBook); 
+            threads[i].Name = $"Thread-{i+1}"; 
+            threads[i].Start();
+        }
+
+        foreach (var thread in threads) 
+        {
+            thread.Join();
+        }
+
+        Console.WriteLine("Book Ticket Ran");
+    }
+
+
+
+
+
+}
